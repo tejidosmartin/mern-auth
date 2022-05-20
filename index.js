f1892c450762295c643c8862fc84893e043e9f58
shakarr <risaaflo@gmail.com>
2022-05-20 20:17:52 +0200

    new build

--- index.js
+++ index.js
@@ -15,15 +15,6 @@ app.use(express.static("public"));
 //cors
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 //lectura y parse del body
 app.use(express.json());
 
