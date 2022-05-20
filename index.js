bb8d81210570ecfc3f26bcb0d8899fe6cb51b6bb
shakarr <risaaflo@gmail.com>
2022-05-20 18:47:40 +0200

    new build

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const cors = require('cors')
-const { dbConnection } = require('./db/config')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("./db/config");
+require("dotenv").config();
 
 //crear el servidor/app de express
-const app = express()
+const app = express();
 
 //db
-dbConnection()
+dbConnection();
 
 //directorio publico
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //cors
-app.use(cors())
+app.use(cors({ origin: true, credentials: true }));
 
 //lectura y parse del body
-app.use(express.json())
+app.use(express.json());
 
 //Rutas
-app.use('/api/auth', require('./routes/auth'))
+app.use("/api/auth", require("./routes/auth"));
 
 app.listen(process.env.PORT, () => {
-    console.log(`up in ${process.env.PORT}`);
-})
+  console.log(`up in ${process.env.PORT}`);
+});
